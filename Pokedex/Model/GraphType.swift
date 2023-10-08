0fda1332c98acef6fa0e3bfb44f6f947be55ba2d
Jules PASCUAL-RAMON <jules.pascualramon.sio2020@gmail.com>
2023-10-08 16:04:54 +0200

    pokemon sexe repartition charts

---
+++ Pokedex/Model/GraphType.swift
@@ -0,0 +1,14 @@
+//
+//  GraphType.swift
+//  Pokedex
+//
+//  Created by Jules PASCUAL-RAMON on 08/10/2023.
+//
+
+import SwiftUI
+
+enum GraphType: String, CaseIterable {
+    case donut = "Donut Chart"
+    case pie = "Pie Chart"
+    case bar = "Bar Chart"
+}
